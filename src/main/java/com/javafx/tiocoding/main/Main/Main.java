aa75ee3c3df2f1a5c908aebc2b723b34c3159eb9
pipe <feliperv@gmail.com>
2019-04-14 20:32:42 -0500

    iniciamos primer commit

---
+++ src/main/java/com/javafx/tiocoding/main/Main/Main.java
@@ -0,0 +1,21 @@
+package com.javafx.tiocoding.main.Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	
+		
+	
+	}
+	
+	public static void main(String []a) {
+		
+		launch(a);
+	}
+	
+
+}
